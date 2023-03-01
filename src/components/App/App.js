5387031f77ce6c5a474fcae6aa0c7cef2d4a079c
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-28 18:37:21 -0700

    Add artist API functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { Component } from 'react';
+import Form from '../Form/Form';
+import Artists from '../Artists/Artists';
+import getArtistAPICalls from '../../APICalls';
 
 class App extends Component {
   constructor() {
@@ -8,6 +11,13 @@ class App extends Component {
       artists: []
     }
   }
+
+  componentDidMount = () => {
+    getArtistAPICalls('artists')
+    .then((data) => this.setState({ artists: data.artists }))
+    .catch((error) => console.log(error))
+  }
+
   render() {
     return(
       <></>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,3 +16,5 @@ class Form extends Component {
     )
   }
 }
+
+export default Form 
