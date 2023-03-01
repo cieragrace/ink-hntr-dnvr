f54d172e2901317ce458e08f294e850863c0c381
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-28 19:14:11 -0700

    Add router to index

--- src/components/ArtistCard/ArtistCard.js
+++ src/components/ArtistCard/ArtistCard.js
@@ -1,7 +1,7 @@
 import './ArtistCard.css'
 import React from 'react'
 
-const ArtistCard = () => {
+const ArtistCard = (props) => {
   return(
     //navlink below with 1 image
     <></>
--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,9 +1,25 @@
 import './Artists.css'
 import React from 'react'
+import ArtistCard from '../ArtistCard/ArtistCard'
 
-const Artists = () => {
+const Artists = (props) => {
+  const artistCards = props.artists.map(artist => {
   return(
-    <></>
+    <ArtistCard 
+      id={artist.id}
+      key={artist.id}
+      name={artist.name}
+      shop={artist.shop}
+      img={artist.img[2]}
+      keywords={artist.keywords}
+      phone={artist.phone}
+    />
+  )
+})
+  return(
+    <div>
+      {artistCards}
+    </div>
   )
 }
 
