223f49f0c320a8bd83ec9781ee6f7b84a2b4ceb4
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-28 17:49:53 -0700

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super ()
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <></>
+    )
+  }
+}
+
+export default App;
