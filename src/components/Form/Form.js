732fdb4d3f173c67d8681dc84107abdd495a3ebe
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-28 18:26:21 -0700

    Add structure to components

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,1 +1,18 @@
 import './Form.css'
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input1Value: '',
+      input2Value: '',
+      input3Value: ''
+    }
+  }
+  render() {
+    return(
+      <></>
+    )
+  }
+}
