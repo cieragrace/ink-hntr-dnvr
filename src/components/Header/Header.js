2d9e48d117480018d0a4291e1e112b1190a8685f
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-03-04 10:22:24 -0700

    Add header component to eliminate form everywhere

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css'
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return(
+    <div className="header-container">
+      <Link to='/' className="header-logo"></Link>
+      <Link to='/' className='home-btn'>Back To Home</Link>
+    </div>
+  )
+}
+
+export default Header
